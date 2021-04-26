ea6c4d3f6a4f9cf2561e07d0aa61190e1bbf93a2
Katsumi <kmorimatsu@users.sourceforge.jp>
2021-04-26 11:03:00 -0700

    Go back to automatic mode of ERN30 Ymax

--- bargraphs.js
+++ bargraphs.js
@@ -62,7 +62,7 @@ draw_graphs=function(viewPercent){
 		labels.push(gdata[i]['pref']);
 		data2.push(gdata[i]['ern30']);
 	}
-	draw_bargraph('bargraph3.7',labels,date+'の実行再生産数比較（陽性者30人以上の都道府県）','実行再生産数',data2,0,'',5);
+	draw_bargraph('bargraph3.7',labels,date+'の実行再生産数比較（陽性者30人以上の都道府県）','実行再生産数',data2,0,'');
 	// Sort array for positives in last week per population
 	gdata.sort(function(a,b){ return b.positivesppw-a.positivesppw; });
 	labels=new Array();
